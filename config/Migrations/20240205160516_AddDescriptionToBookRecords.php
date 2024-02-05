32560971875329c29b278ada2645d58e5b98ef0c
Agata-M-S <agata.webdevelopment@gmail.com>
2024-02-05 20:27:57 +0000

    add description collumn to book records table

---
+++ config/Migrations/20240205160516_AddDescriptionToBookRecords.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddDescriptionToBookRecords extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+      $table = $this->table('book_records');
+      $table-> addColumn('description', 'string', [
+        'default'=> 'book description',
+        'limit' => 250,
+        'null'=> false
+      ]);
+      $table-> update();
+    }
+}
